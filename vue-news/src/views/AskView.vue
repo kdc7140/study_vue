0981e5408a1b53d67ebc50e6ca0bbb82617ed377
KDC <kdc7140@naver.com>
2022-11-03 09:11:00 +0900

    axios install and add code sample

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,12 +1,29 @@
 <template>
     <div>
-        ask
+        <div v-for='ask in askList'>{{ ask.title }}</div>
     </div>
 </template>
 
 <script>
-export default {
 
+import { fetchAskList } from '../api/index.js';
+
+export default {
+  data(){
+    return{
+      askList : []
+    }
+  },
+  created(){
+    fetchAskList()
+      .then(response => {
+        console.log(response);
+        this.askList = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
 }
 </script>
 
--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
-    Jobs
+    <div v-for='jobs in jobsList'>{{ jobs.title }}</div>
   </div>
 </template>
 
 <script>
-export default {
 
+import { fetchJobsList } from '../api/index.js';
+
+export default {
+  data(){
+    return{
+      jobsList : []
+    }
+  },
+  created(){
+    fetchJobsList()
+      .then(response => {
+        console.log(response);
+        this.jobsList = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
 }
 </script>
 
